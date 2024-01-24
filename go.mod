ffabcd0aa4f802530048341c04ac718b8b41d54d
Hongwei Liu <hongliu@redhat.com>
2024-01-24 16:09:24 +0800

    Add gitlab-api

---
+++ go.mod
@@ -0,0 +1,18 @@
+module gitlab-api
+
+go 1.20
+
+require github.com/xanzy/go-gitlab v0.95.2
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.5 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	golang.org/x/oauth2 v0.14.0 // indirect
+	golang.org/x/time v0.4.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+
+	gitlab "github.com/xanzy/go-gitlab"
+)
+
+var (
+	gitClient  *gitlab.Client
+	token      string = "XXXXXXX"
+	url        string = "https://gitlab.cee.redhat.com"
+	projectID  int    = 84399
+	mrID       int    = 4
+	sha        string = "8123e58477ccb2d5ed50cf9e09b8c72831722c8a"
+	targetUrl  string = "https://console.redhat.com/preview/application-pipeline/workspaces/hongweiliu/applications/go-test/pipelineruns/go-test-mk45c-bvprc"
+	desciption string = "Failed"
+	name       string = "RHTAP Integration test - scenario 1 - snapshot 1"
+	context    string = "mr 14"
+)
+
+func main() {
+	var err error
+	gitClient, err = gitlab.NewClient(token, gitlab.WithBaseURL(url))
+	if err != nil {
+		fmt.Printf("initclienterr:%v\n", err)
+		panic(err)
+	} else {
+		fmt.Println("initialize...")
+	}
+
+	opt := &gitlab.SetCommitStatusOptions{
+		State:       gitlab.BuildStateValue("failed"),
+		Name:        gitlab.Ptr(name),
+		TargetURL:   gitlab.Ptr(targetUrl),
+		Description: gitlab.Ptr(desciption),
+	}
+
+	//Create CommitStatus for commit
+	commitStatus, _, err := gitClient.Commits.SetCommitStatus(projectID, sha, opt)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Printf("succeed to create commitStatus with %d...\n", commitStatus.PipelineId)
+	}
+
+	//Update CommitStatus for Pipeline created above
+	opt = &gitlab.SetCommitStatusOptions{
+		State:       gitlab.BuildStateValue("success"),
+		Name:        gitlab.Ptr(name),
+		TargetURL:   gitlab.Ptr(targetUrl),
+		Description: gitlab.Ptr(desciption),
+		PipelineID:  gitlab.Ptr(commitStatus.PipelineId),
+	}
+	commitStatus, _, err = gitClient.Commits.SetCommitStatus(projectID, sha, opt)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Printf("succeed to update commitStatus %d\n", commitStatus.PipelineId)
+	}
+
+	//Create a note for merge request
+	body := `Integration test for snapshot snapshot 1 and scenario 1 has <b>Failed</b>
+
+| Task | Duration | Test Suite | Status | Details 
+|----------|----------|----------|----------|----------|
+| task 1 |28 seconds | Default | :x: Failed | :white_check_mark: 5 success(es) <br> :warning: 1 warning(s) <br> :x: 1 failure(s) |
+| task 2 |3 minutes  | Default | :white_check_mark: Success | :white_check_mark: 80 success(es) <br> :warning: 3 warning(s)|
+| task 3 |2 seconds  | Default | :warning: Warning | :white_check_mark: 10 success(es) <br> :warning: 1 warning(s)`
+
+	mopt_create := &gitlab.CreateMergeRequestNoteOptions{Body: gitlab.Ptr(body)}
+	comment, _, err := gitClient.Notes.CreateMergeRequestNote(projectID, mrID, mopt_create)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Printf("succeed to create comment %d\n", comment.ID)
+	}
+
+	mopt_update := &gitlab.UpdateMergeRequestNoteOptions{Body: gitlab.Ptr(body)}
+	_, _, err = gitClient.Notes.UpdateMergeRequestNote(projectID, mrID, comment.ID, mopt_update)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Printf("succeed to update comment %d\n", comment.ID)
+	}
+}
